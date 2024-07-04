56266daa866e82a85218c17ee048ffdb7bfeeddc
DiyaGoyal0212 <goyal.diya0212@gmail.com>
2024-07-05 00:21:17 +0530

    Co-authored-by: Kaushaki Jha <kaushakijha@users.noreply.github.com>

--- comp/src/App.js
+++ comp/src/App.js
@@ -1,10 +1,22 @@
 import First from './components/First';
+import {BrowserRouter , Routes , Route} from "react-router-dom";
 import './App.css';
+import AdminLogin from './components/AdminLogin';
 
 function App() {
   return (
     <>
-    <First />
+    <BrowserRouter>
+   
+    <Routes>
+        <Route path='/' element={<First/>} />
+        <Route path='/AdminLogin' element={<AdminLogin/>} />
+        
+       
+        
+    </Routes>
+   
+    </BrowserRouter>
     </>
   );
 }
--- comp/src/components/First.jsx
+++ comp/src/components/First.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import "./First.css";
+import { NavLink } from 'react-router-dom';
 
 export default function First() {
   return (
     <div>
-        <h1>hi</h1>
+      <div className='cont'>
+        <h1><u>UComplaint</u></h1>
+        <div className='info'>
+          <div className='stud'>
+            <img src="../images/final_stud.png"></img>
+            <h2>LOGIN AS</h2>
+            <button> <h3>Student</h3></button>
+
+          </div>
+          <div className="admin">
+          <img src="../images/final_admin.webp"></img>
+          <h2>LOGIN AS</h2>
+          <NavLink to="/AdminLogin"><button><h3>Admin</h3></button></NavLink>
+          </div>
+
+        </div>
+
+      </div>
+   
       
     </div>
   )
